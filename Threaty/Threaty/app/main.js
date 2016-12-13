3963af4ca42c446fc4f0601e6a3a62e1600dbdf7
mehrandvd@yahoo.com <popoldvd20>
2016-12-13 16:18:43 +0330

    durandal initialized.

---
+++ Threaty/Threaty/app/main.js
@@ -0,0 +1,39 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal':'../Scripts/durandal',
+        'plugins' : '../Scripts/durandal/plugins',
+        'transitions' : '../Scripts/durandal/transitions',
+        'knockout': '../Scripts/knockout-3.4.0',
+        'bootstrap': '../Scripts/bootstrap',
+        'jquery': '../Scripts/jquery-1.9.1'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+       }
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Starter Kit';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
