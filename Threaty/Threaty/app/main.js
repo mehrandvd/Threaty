5a3f53128a4a052d3c39c98faf735947782e1ee0
mehrandvd@yahoo.com <popoldvd20>
2016-12-13 16:44:15 +0330

    Branding Threaty and bootstrap-flaty

--- Threaty/Threaty/app/main.js
+++ Threaty/Threaty/app/main.js
@@ -21,7 +21,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'],
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Durandal Starter Kit';
+    app.title = 'Threaty';
 
     app.configurePlugins({
         router:true,
--- Threaty/Threaty/app/viewmodels/shell.js
+++ Threaty/Threaty/app/viewmodels/shell.js
@@ -8,7 +8,7 @@
         },
         activate: function () {
             router.map([
-                { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
+                { route: '', title:'Home', moduleId: 'viewmodels/home', nav: true },
                 { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
             ]).buildNavigationModel();
             
