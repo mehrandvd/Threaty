f155f292d74449ae1387c5dffc435460967d8b6e
mehrandvd@yahoo.com <popoldvd20>
2016-12-20 17:15:18 +0330

    init tracer page

--- Threaty/Threaty/app/main.js
+++ Threaty/Threaty/app/main.js
@@ -16,13 +16,33 @@
     }
 });
 
+function configToastr() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+}
+
 define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'bootstrap'],  function (system, app, viewLocator) {
     //>>excludeStart("build", true);
     system.debug(true);
     //>>excludeEnd("build");
-
+    configToastr();
     app.title = 'Threaty';
-
+    
     app.configurePlugins({
         router:true,
         dialog: true
